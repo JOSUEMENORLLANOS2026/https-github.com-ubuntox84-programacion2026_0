a03dcf0993ce35da2af419c931f7720e84a59d5d
josuemenorllanos <menorllanosjharleyjosue@gmail.com>
2026-01-26 11:58:34 -0500

    cambios

--- semana 1/TALLER VIERNES/ejercicio.2.tv.cpp
+++ semana 1/TALLER VIERNES/ejercicio.2.tv.cpp
@@ -10,12 +10,12 @@ int main()
      cin >> altura;
      cout << "........calcular el area:......... " << endl;
      area = base * altura;
-     cout << "el area del cuadrado es: " << area << endl;
+     cout << "el area del rectangulo es: " << area << endl;
      cout << ".....calcular el perimetro:.... " << endl;
      perimetro = 2 * (base + altura);
      cout << "el perimetro es: " << perimetro << endl;
      cout << "......calcular la diagonal principal:..." << endl;
      diagonal = sqrt(pow(base,2) + pow(altura,2));
-     cout << "La diagonal del cuadrado es: " << diagonal << endl;
+     cout << "La diagonal del rectangulo es: " << diagonal << endl;
      return 0;
 }
